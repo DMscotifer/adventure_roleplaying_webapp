ee0d366db166d07108af20b73b976f3ea9d1d418
Scott Stevenson <scotifer92@googlemail.com>
2018-04-22 15:13:44 +0100

    item save function added

--- models/item.rb
+++ models/item.rb
@@ -3,13 +3,30 @@ require_relative("../db/sql_runner")
 class Item
 
   attr_reader :id
-  attr_accessor :monster_id, :player_id
+  attr_accessor :name, :monster_id, :player_id
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
+    @name = options['name']
     @monster_id = options['monster_id'].to_i
     @player_id = options['player_id'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO items
+    (
+      name,
+      monster_id,
+      player_id
+    )
+    VALUES
+    (
+      $1, $2, $3
+    )
+    RETURNING id"
+    values = [@name, @monster_id, @player_id]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+  end
   
 end
