1aaa5f85216d2e95b7f8581982efd57528d1e7b5
Scott Stevenson <scotifer92@googlemail.com>
2018-04-25 09:23:03 +0100

    new monster and new character added, but obstructed by CSS

--- controllers/monster_controller.rb
+++ controllers/monster_controller.rb
@@ -8,6 +8,10 @@ get '/monsters' do
   erb ( :"monsters/index")
 end
 
+get "/monsters/new" do
+  erb(:"monsters/new")
+end
+
 get '/monsters/dr' do
   @monsters = Monster.filter_by_dr(params['dr'].to_i)
   erb (:"monsters/dr")
--- controllers/player_controller.rb
+++ controllers/player_controller.rb
@@ -11,13 +11,13 @@ get '/players' do
   erb ( :"players/index")
 end
 
+get "/players/new" do
+  erb(:"players/new")
+end
+
 get '/players/:id' do
   @player = Player.find(params['id'].to_i)
   erb(:"players/show")
-end
-
-get "/players/new" do
-  erb(:new)
 end
 
 post '/' do
